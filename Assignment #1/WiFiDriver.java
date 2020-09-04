f7a4cbdf42f66729ff56934df56f18a96f532b32
Cromwell N <64291044+CromwellN@users.noreply.github.com>
2020-09-03 22:22:06 -0400

    Update WiFiDriver.java

--- Assignment #1/WiFiDriver.java
+++ Assignment #1/WiFiDriver.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
-/*
-Class: CMSC203 CRN 22445
-Program: Assignment 1 
-Instructor: Dr. Grinberg
+/**
+*Class: CMSC203 CRN 22445
+*Program: Assignment 1 
+*Instructor: Dr.Grinberg
 Summary of Description: A program that go through some possible problem that restore the Wifi connectivity.
-Due Date: 09/13/2020
-Integrity Pledge: I pledge that I have completed the programming assignment independently.
+*Due Date: 09/13/2020
+*Integrity Pledge: I pledge that I have completed the programming assignment independently.
 I have not copied the code from a student or any source.
 */
-
 public class WiFiDriver {
 
 	public static void main(String[] args) {
 		System.out.println("If you have a problem with internet connectivity, this WiFi Diagnosis might work.");
-		
+
 		System.out.println("\nReboot the computer and try to connect");
 		Scanner myobj = new Scanner(System.in);
-		
-		System.out.println("\nDid that fix the problem? (yes/No) :");
+		//Prompt the user to enter yes or no
+		System.out.println("\nDid that fix the problem? (yes/no) :");
 		String userInput = myobj.nextLine();
+		
+		
 		if(userInput.equals("yes")) {
 			System.out.println("Rebooting your computer seemed to work");
 			System.exit(0);
 		}
 		else{
 			System.out.println("Reboot the computer and try to connect");
-			System.out.println("\nDid that fix the problem? (yes/No) :" );
+			System.out.println("\nDid that fix the problem? (yes/no) :" );
 			userInput = myobj.nextLine();
 			if(userInput.equals("yes")) {
 				System.out.println("Rebooting your computer seemed to work");
@@ -33,7 +34,7 @@ public class WiFiDriver {
 			}
 			else{
 				System.out.println("Make sure the cables connecting the router are fimly plugged in and power is getting to the router");
-				System.out.println("\nDid that fix the problem? (yes/No) :" );
+				System.out.println("\nDid that fix the problem? (yes/no) :" );
 				userInput = myobj.nextLine();
 				if(userInput.equals("yes")) {
 					System.out.println("Checking the router's cables seemed to work");
@@ -41,7 +42,7 @@ public class WiFiDriver {
 				}
 				else {
 					System.out.println("\nMove the computer closer to the router and try to connect");
-					System.out.println("\nDid that fix the problem? (yes/No) :" );
+					System.out.println("\nDid that fix the problem? (yes/no) :" );
 					userInput = myobj.nextLine();
 					if (userInput.equals("yes")) {
 						System.out.println("Moving the router's cables seemed to work");
@@ -51,13 +52,9 @@ public class WiFiDriver {
 						System.out.println("\nContact your ISP");
 						System.out.println("Make sure your ISP is hooked up to your router.");
 						System.exit(0);
-					}
-					
+					}	
 				}
-			
-			}
-			
+			}	
 		}
 	}
-
 }
